d14bcaca96fb2a3078e53b0499503197dd472bbf
Max <Max@Home>
2022-07-13 19:31:48 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Forms = System.Windows.Forms;
+
+namespace WindowToggler2000
+{
+    //Mish mash of these:
+    //https://www.thomasclaudiushuber.com/2015/08/22/creating-a-background-application-with-wpf/
+    //https://www.youtube.com/watch?v=8NuoBe1HS0U
+    //nuget needed for windows.forms in .net 6: https://github.com/hardcodet/wpf-notifyicon/blob/develop/README.md
+    public partial class App : Application
+    {
+        private readonly Forms.NotifyIcon _notifyIcon;
+        private bool _isExit;
+        public App() {_notifyIcon = new Forms.NotifyIcon();}
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+            MainWindow.Hide();
+
+            MainWindow.Closing += MainWindowClosing;
+
+
+            _notifyIcon.Icon = new System.Drawing.Icon("Resources/Icon.ico");
+            _notifyIcon.Text = "Sanck is running";
+            _notifyIcon.DoubleClick += (s, args) => NotifyIcon_Click();
+
+
+            _notifyIcon.ContextMenuStrip = new Forms.ContextMenuStrip();
+            _notifyIcon.Visible = true;
+            _notifyIcon.ContextMenuStrip.Items.Add("Exit").Click += (s, e) => ExitApplication();
+        }
+        private void ExitApplication()
+        {
+            _isExit = true;
+            MainWindow.Close();
+            _notifyIcon.Dispose();
+        }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _notifyIcon.Dispose();
+            base.OnExit(e);
+        }
+        private void NotifyIcon_Click()
+        {
+            if (MainWindow.IsVisible)
+            {
+                if (MainWindow.WindowState == WindowState.Minimized)
+                {
+                    MainWindow.WindowState = WindowState.Normal;
+                }
+                MainWindow.Activate();
+            }
+            else MainWindow.Show();
+
+        }
+
+        private void MainWindowClosing(object sender, CancelEventArgs e)
+        {
+            if (!_isExit)
+            {
+                e.Cancel = true;
+                MainWindow.Hide();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Windows;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Diagnostics;
+
+namespace WindowToggler2000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    //https://blog.magnusmontin.net/2015/03/31/implementing-global-hot-keys-in-wpf/
+    public partial class MainWindow : Window
+    {
+
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int SW_MAXIMIZE = 3;
+        private const int SW_MINIMIZE = 6;
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        [DllImport("user32.dll")]
+        public static extern bool IsIconic(IntPtr handle);
+
+        private const int HOTKEY_ID = 9000;
+
+        //Modifiers:
+        private const uint MOD_NONE = 0x0000; //(none)
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint MOD_CONTROL = 0x0002; //CTRL
+        private const uint MOD_SHIFT = 0x0004; //SHIFT
+        private const uint MOD_WIN = 0x0008; //WINDOWS
+        //CAPS LOCK:
+        private const uint VK_CUSTOM = 0xE2;
+
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+        }
+
+        private IntPtr _windowHandle;
+        private HwndSource _source;
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            _windowHandle = new WindowInteropHelper(this).Handle;
+            _source = HwndSource.FromHwnd(_windowHandle);
+            _source.AddHook(HwndHook);
+
+            RegisterHotKey(_windowHandle, HOTKEY_ID, MOD_CONTROL, VK_CUSTOM); //CTRL + CAPS_LOCK
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_CUSTOM)
+                            {
+                                //https://stackoverflow.com/a/20470454
+                                Process[] processes = Process.GetProcesses();
+                                var targetWindow = "discord";
+                                foreach (Process p in processes)
+                                {
+                                    if (p.MainWindowTitle.ToLower().Contains(targetWindow))
+                                    {
+                                        if (IsIconic(p.MainWindowHandle)) ShowWindow(p.MainWindowHandle, SW_MAXIMIZE);
+                                        else ShowWindow(p.MainWindowHandle, SW_MINIMIZE);
+                                        break;
+                                    }
+                                }
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            _source.RemoveHook(HwndHook);
+            UnregisterHotKey(_windowHandle, HOTKEY_ID);
+            base.OnClosed(e);
+        }
+    }
+}
